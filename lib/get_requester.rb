d563a7fe2bc7b1b8e3c228535a103bf346ac8a68
Kyle Samuel <Ksamuel92@gmail.com>
2021-03-24 11:33:12 -0400

    commit tree e1e117cd6527e20a0d4128836419553a075c23e5

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,25 @@
 # Write your code here
+require 'open-uri'
+require 'json'
+require 'net/http'
+
+class GetRequester
+    attr_accessor :stringURL
+
+    def initialize(stringURL)
+        @stringURL = stringURL
+    end
+
+    def get_response_body
+    #sends GET request to the url passed in
+        uri = URI.parse(stringURL)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+    JSON.parse(self.get_response_body)
+    end
+
+   
+end
